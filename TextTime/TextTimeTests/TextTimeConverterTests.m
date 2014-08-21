4535d850860bee3e095ee0536d41364c95282534
Bjarte Skjørestad <bjarte@Bjartes-MacBook-Pro.local>
2014-08-21 15:37:30 +0200

    Added first test

---
+++ TextTime/TextTimeTests/TextTimeConverterTests.m
@@ -0,0 +1,23 @@
+//
+//  TextTimeConverterTests.m
+//  TextTime
+//
+//  Created by Bjarte Skjørestad on 21/08/14.
+//  Copyright (c) 2014 Bjarte Skjørestad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "TextTimeConverter.h"
+
+@interface TextTimeConverterTests : XCTestCase
+
+@end
+
+@implementation TextTimeConverterTests
+- (void)testZeroReturnsOClock {
+    TextTimeConverter *converter = [[TextTimeConverter alloc] init];
+    NSString *result = [converter convertNumber:@0];
+    XCTAssertEqual(result, @"0");
+}
+@end
